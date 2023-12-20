ed94fd32d4f648bce6eb1bc53ccf6ea630c9802d
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-20 13:53:12 -0300

    Adicionando os videos da api a nossa pagina com template string no innerHTML

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const api = fetch("http://localhost:3000/videos")
+const containerVideos = document.querySelector(".videos__container");
 // resposta - resposta do status da promisse
-// A função json() é usada para extrair e analisar o corpo de uma resposta HTTP que está no formato JSON (JavaScript Object Notation).
-.then(resposta => console.log(resposta.json()))
+// json() extrai o corpo de uma resposta HTTP que está no formato JSON
+const api = fetch("http://localhost:3000/videos")
+    .then(resposta => resposta.json())
+    .then((videos) =>
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `
+            <li class="videos__item"><iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src"${video.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}</p>
+                </div>
+            </li>`;
+        })
+    )
