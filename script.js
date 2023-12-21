d07db7257af4e612e5080b4306f2c0822632169a
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-21 09:44:52 -0300

    Deixando nosso código mais legivel e moderno, com função assincrona, facilitando a manutenção com o async/await

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const containerVideos = document.querySelector(".videos__container");
-// resposta - resposta do status da promisse
-// json() extrai o corpo de uma resposta HTTP que está no formato JSON
-const api = fetch("http://localhost:3000/videos")
-    .then(resposta => resposta.json())
-    .then((videos) =>
-        videos.forEach((video) => {
-            containerVideos.innerHTML += `
+
+// Maneira mais moderna e legível
+// Indicando ao js que estamos lidando com operações assincronas
+async function buscarEMostrarVideos() {
+    // Callback é uma função que é passada como argumento para outra função e é executada após a conclusão de uma operação assíncrona.
+    const busca = await fetch("http://localhost:3000/videos") // await aguarda até que a busca seja feita
+    const videos = await busca.json();
+    videos.forEach((video) => {
+        // Corpo da segunda callback
+        containerVideos.innerHTML += `
             <li class="videos__item"><iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
                     <img class="img-canal" src"${video.imagem}" alt="Logo do Canal">
@@ -13,9 +16,7 @@ const api = fetch("http://localhost:3000/videos")
                     <p class="titulo-canal">${video.descricao}</p>
                 </div>
             </li>`;
-        })
-    )
-    .catch((erro) => {
-        console.log('aconteceu um erro')
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${erro}</p>`
     })
+}
+
+buscarEMostrarVideos();
